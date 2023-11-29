fb27ffd0173ac1c68dc6d8ca40b41b0b43d04bb6
Park <tanagrit.sornchaiyagorn@gmail.com>
2023-11-29 13:36:28 +0700

    addt(park):add and adjust function in various type function to ex1 - 6 files

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,31 @@
-// Start coding here
+let add = function (a, b) {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a + b;
+  } else {
+    return "please put number type argument";
+  }
+};
+
+let subtract = function (a, b) {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a - b;
+  } else {
+    return "please put number type argument";
+  }
+};
+
+let multiply = function (a, b) {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a * b;
+  } else {
+    return "please put number type argument";
+  }
+};
+
+let divide = function (a, b) {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a / b;
+  } else {
+    return "please put number type argument";
+  }
+};
--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,37 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+  add: (a, b) => {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+      return a + b;
+    } else {
+      return "please put number type argument";
+    }
+  },
+  subtract: (a, b) => {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+      return a - b;
+    } else {
+      return "please put number type argument";
+    }
+  },
+  multiply: (a, b) => {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+      return a * b;
+    } else {
+      return "please put number type argument";
+    }
+  },
+  divide: (a, b) => {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+      return a / b;
+    } else {
+      return "please put number type argument";
+    }
+  },
+};
+
+let addResult = calculator.add(10, 20);
+console.log(addResult);
+
+let divideResult = calculator.divide(3000, 10);
+console.log(divideResult);
